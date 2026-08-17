85f2786c364147ca117d927a03e5ab2d31af7e78
wintkhantlin <happer64bit@gmail.com>
2026-08-17 08:52:42 +0630

    refactor: remove unused files and code related to authentication and routing
    - Deleted constants, environment definitions, and axios configuration files.
    - Removed authentication store and related logic for user sessions.
    - Eliminated filter and todo stores, along with their associated logic.
    - Cleared out views related to user creation, sign-in, password reset, and home view.
    - Removed TypeScript configuration files and Vite configuration for the web app.

--- apps/web-app/src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const API_GATEWAY = import.meta.env.VITE_API_GATEWAY ?? "http://localhost:3001";
-
--- apps/web-app/src/types/auth.type.ts
+++
@@ -1,5 +0,0 @@
-export interface UserSession {
-    uid: string
-    username: string
-    email: string
-}
